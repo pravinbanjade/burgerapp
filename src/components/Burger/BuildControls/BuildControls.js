c1436e019e5e1b5979d4b1c469eb09775f6a6a0f
Pravin Banjade <pravinbanjade@github.com>
2018-07-24 21:21:21 +0545

    added multiple build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const buildControls = (props) => (
-  <div>
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
+const controls = [
+  { label: 'Salad', types: 'salad' },
+  { label: 'Bacon', types: 'bacon' },
+  { label: 'Cheese', types: 'cheese' },
+  { label: 'Meat', types: 'meat' }
+]
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map( ctrl => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
   </div>
 )
 
