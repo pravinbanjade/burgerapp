d03bb1230d1aa980f3a2dcdb923335af225d65bc
root <root@unknown.localdomain>
2018-07-24 11:49:21 +0545

    starting implementation of burger app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxi';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
     <div> Toolbar, SideDrawer, BackDrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxi';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Burger Controls</div>
+      </Aux>
+    )
+  }
+}
+
+export default BurgerBuilder;
