486ba37972582366938233b193d58ea97601b18e
Pravin Banjade <pravinbanjade@github.com>
2018-07-28 17:44:56 +0545

    added global Error Handler in burger builder

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Spinner.css';
 
 const spinner = () => (
-  <div className={classes.Spinner}>Loading...</div>
+  <div className={classes.Spinner} />
 );
 
 export default spinner;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICE = {
   salad: 30,
@@ -17,16 +18,22 @@ const INGREDIENT_PRICE = {
 
 class BurgerBuilder extends Component {
   state = {
-    ingredient : {
-      salad: 0,
-      bacon: 0,
-      cheese: 0,
-      meat: 0,
-    },
+    ingredient : null,
     totalPrice: 30,
     purchasable: false,
     purchasing: false,
-    loading: false
+    loading: false,
+    error: false
+  }
+
+  componentDidMount() {
+    axios.get('https://react-burger-builder-786.firebaseio.com/ingredient.json')
+      .then( res => {
+        this.setState({ ingredient: res.data });
+      })
+      .catch( error => {
+        this.setState({ error: true })
+      });
   }
 
   updatePurchaseState(ingredient) {
@@ -111,11 +118,28 @@ class BurgerBuilder extends Component {
     for(let key in disabledInfo) {
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
-    let orderSummary = <OrderSummary
-      ingredient={this.state.ingredient}
-      purchaseCancelled={this.purchaseCancelHandler}
-      purchaseSucceed={this.purchaseSuccessHandler}
-      price={this.state.totalPrice} />;
+    let orderSummary = null;
+    let burger = this.state.error ? <p>Ingredients can't be loaded.</p> : <Spinner />
+    if(this.state.ingredient) {
+      burger = (
+        <Aux>
+          <Burger ingredient={this.state.ingredient} />
+          <BuildControls
+            ingredientAdded={this.addIngredientHandler}
+            ingredientRemoved={this.removeIngredientHandler}
+            disabled={disabledInfo}
+            ordered={this.purchaseHandler}
+            price={this.state.totalPrice}
+            purchasable={this.state.purchasable}
+          />
+        </Aux>
+      );
+      orderSummary = <OrderSummary
+        ingredient={this.state.ingredient}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseSucceed={this.purchaseSuccessHandler}
+        price={this.state.totalPrice} />;
+    }
     if(this.state.loading) {
       orderSummary = <Spinner />
     }
@@ -124,18 +148,10 @@ class BurgerBuilder extends Component {
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           {orderSummary}
         </Modal>
-        <Burger ingredient={this.state.ingredient} />
-        <BuildControls
-          ingredientAdded={this.addIngredientHandler}
-          ingredientRemoved={this.removeIngredientHandler}
-          disabled={disabledInfo}
-          ordered={this.purchaseHandler}
-          price={this.state.totalPrice}
-          purchasable={this.state.purchasable}
-        />
+        {burger}
       </Aux>
     )
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
