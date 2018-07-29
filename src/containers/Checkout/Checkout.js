6c74a97b936da9c7316417b87d26a4b2579c4c9d
Pravin Banjade <pravinbanjade@github.com>
2018-07-29 19:42:32 +0545

    passing ingredients from query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,10 +12,31 @@ class Checkout extends Component {
     }
   }
 
+  componentDidMount() {
+    console.log(this.props);
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredient = {};
+    for(let param of query.entries()) {
+      ingredient[param[0]] = +param[1];
+    }
+    this.setState({ ingredient: ingredient });
+  }
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
   render() {
     return(
       <div>
-        <CheckoutSummary ingredient={this.state.ingredient} />
+        <CheckoutSummary
+          ingredient={this.state.ingredient}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler} />
       </div>
     );
   }
