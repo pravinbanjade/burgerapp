99e3938d731bdffe87d8894669a7e218ec836b9b
Pravin Banjade <pravinbanjade@github.com>
2018-08-01 17:21:40 +0545

    bug fixed in form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -105,7 +105,8 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'}
           ]
         },
-        value: '',
+        value: 'fastest',
+        validation: {},
         valid: true
       },
     },
