e80467ac77caf5b855873fc37183ee52a2a0afe5
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-03 15:34:07 +0545

    connecting redux with react

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes form './actions';
+
+const initialState = {
+  ingredient : null,
+  totalPrice: 30,
+}
+
+const reducer = (state = initialState, action) => {
+
+}
+
+export default reducer;
