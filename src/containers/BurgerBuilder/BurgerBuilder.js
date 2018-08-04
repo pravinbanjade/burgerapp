34e8f37ae029e66fe912e76f77a88289bf9d6fba
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-04 18:37:01 +0545

    fixed purchasable button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -35,7 +34,7 @@ class BurgerBuilder extends Component {
     }).reduce( (sum, el) => {
       return sum + el;
     },0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -79,7 +78,7 @@ class BurgerBuilder extends Component {
             disabled={disabledInfo}
             ordered={this.purchaseHandler}
             price={this.props.price}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
           />
         </Aux>
       );
