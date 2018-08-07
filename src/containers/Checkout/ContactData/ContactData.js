92c6a228e24ff65a0c3d589a4596e2e9d5a9a1b9
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-07 11:47:30 +0545

    connected contact data with redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
 
@@ -117,7 +119,6 @@ class ContactData extends Component {
 
   orderHandler = (event) => {
     event.preventDefault({passive: true});
-    this.setState({ loading: true });
     const formData = {};
     for(let formElementIdentifier in this.state.orderForm) {
       formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -127,15 +128,7 @@ class ContactData extends Component {
         price: this.props.price,
         orderData: formData
     };
-
-    axios.post('/orders.json', order)
-        .then( response => {
-          this.setState({ loading: false });
-          this.props.history.push('/');
-        })
-        .catch( error => {
-          this.setState({ loading: false });
-        });
+    this.props.onOrderBurger(order);
   }
 
   checkValidity(value, rules) {
@@ -215,4 +208,10 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+  }
+}
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
