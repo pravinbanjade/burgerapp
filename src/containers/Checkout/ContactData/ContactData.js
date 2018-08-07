f2a68f80765fd68012e38cfdaa1d394053986080
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-07 12:31:24 +0545

    order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -113,8 +113,7 @@ class ContactData extends Component {
         valid: true
       },
     },
-    formIsValid: false,
-    loading: false,
+    formIsValid: false
   }
 
   orderHandler = (event) => {
@@ -189,7 +188,7 @@ class ContactData extends Component {
         <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
-    if(this.state.loading) {
+    if(this.props.loading) {
       form = <Spinner />
     }
     return (
@@ -204,13 +203,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredient,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
   }
 }
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -16,7 +16,13 @@ export const purchaseBurgerFail = (error) => {
   }
 }
 
-export const purchaseBurgerStart = (orderData) => {
+export const purchaseBurgerStart = () => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_START
+  }
+}
+
+export const purchaseBurger = (orderData) => {
   return dispatch => {
     axios.post('/orders.json', orderData)
         .then( response => {
