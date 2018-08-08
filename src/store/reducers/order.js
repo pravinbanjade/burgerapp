ba8186529209a8bb9b272455f3dc34a1da7907e3
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-08 16:36:41 +0545

    fetched orders using redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -28,12 +28,28 @@ const reducer = (state = initialState, action) => {
         loading: false,
         purchased: true,
         orders: state.orders.concat(newOrder)
-      };
+      }
     case actionTypes.PURCHASE_BURGER_FAIL:
       return {
         ...state,
         loading: false
-      };
+      }
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true
+      }
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false
+      }
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return {
+        ...state,
+        loading: false
+      }
     default:
       return state;
   }
