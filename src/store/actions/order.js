592ce10f09099fdade53ca5e324595afeba99c34
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-08 14:36:19 +0545

    fixed not loading while submitting form

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -24,6 +24,7 @@ export const purchaseBurgerStart = () => {
 
 export const purchaseBurger = (orderData) => {
   return dispatch => {
+    dispatch(purchaseBurgerStart());
     axios.post('/orders.json', orderData)
         .then( response => {
           dispatch(purchaseBurgerSuccess(response.data.name, orderData));
