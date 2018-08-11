95aabbe0f024e6b7f9b94aca044949c193432403
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-11 10:55:37 +0545

    storing the token and userId

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import burgerBuilderReduccer from './store/reducers/burgerBuilder';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-  burgerBuilder: burgerBuilderReduccer,
-  order: orderReducer
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+  auth: authReducer
 });
 
 const store = createStore(rootReducer, composeEnhancers(
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   }
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId
   }
 }
 
@@ -37,7 +38,7 @@ export const auth = (email, password, isSignUp) => {
     axios.post(url, authData)
         .then(res => {
             console.log(res);
-            dispatch(authSuccess(res.data));
+            dispatch(authSuccess(res.data.idToken, res.data.localId));
         })
         .catch(err => {
             console.log(err);
