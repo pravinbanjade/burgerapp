d717a72f3ac3681458a46ac5673656ee4c26ea65
Pravin Banjade <pravinbanjade9@gmail.com>
2018-08-12 23:18:37 +0545

    sharing the validation method

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,7 +7,7 @@ import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
-import { updateObject } from '../../shared/utility';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 class Auth extends Component {
 
@@ -47,30 +47,11 @@ class Auth extends Component {
     isSignUp: true
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if(rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-    if(rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if(rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    if(rules.isEmail) {
-      // eslint-disable-next-line
-      const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    return isValid;
-  }
-
   inputChangedHandler = (event, controlName) => {
     const updatedControls = updateObject(this.state.controls, {
         [controlName]: updateObject(this.state.controls[controlName], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+            valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
             touched: true
         })
     });
